55e1ab12e53c1ad37b4e7bffdc1ba55d6c16d492
HannanBhuiyan <hannan.diuh@gmail.com>
2022-04-09 19:09:49 +0600

    even and odd number

---
+++ problem18.py
@@ -0,0 +1,6 @@
+# problem: Find whether a given number is even or odd, print out an appropriate message to the user
+number = int(input())
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
