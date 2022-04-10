13ae985512b36fe374f0abf8c56599e669cefd25
Opedia <hannan.opedia.com>
2022-04-10 12:01:21 +0600

    concatenation string

---
+++ problem24.py
@@ -0,0 +1,11 @@
+
+def concatenate_list_data(items):
+    data = ""
+    for i in items:
+        data += str(i)
+    return data
+
+
+print(concatenate_list_data([1, 2, 3, 4, 5]))
+
+print(concatenate_list_data(['jhon', 'smit', 'marry']))
