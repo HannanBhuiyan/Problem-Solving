68539a4fbccc4501c1ca869a4f1ad00fec323048
Opedia <hannan.opedia.com>
2022-04-10 11:55:32 +0600

    histogram

---
+++ problem23.py
@@ -0,0 +1,15 @@
+
+def histogram(items):
+    for i in items:
+        output = ""
+        times = i
+        while(times > 0):
+            output += "*"
+            times -= 1
+        print(output)
+
+
+histogram([2, 4])
+
+
+
