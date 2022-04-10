c059f79cfc85ee031bef6db9325f5ea667def55c
HannanBhuiyan <hannan.diuh@gmail.com>
2022-04-11 04:30:08 +0600

    triangle area calculate

---
+++ problem27.py
@@ -0,0 +1,5 @@
+# problem : Accept the base and height of a triangle and compute the area
+base = int(input("Enter The base number : "))
+height = int(input("Enter the height number: "))
+area = .5*base*height
+print("area = ", area)
