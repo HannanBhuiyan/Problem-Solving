16e94616a36ec2d17c6a5813fddfb17e916921db
HannanBhuiyan <hannan.diuh@gmail.com>
2022-04-11 22:55:16 +0600

    sum of three numbers

---
+++ problem30.py
@@ -0,0 +1,11 @@
+# problem : Sum of three given integers. However, if two values are equal sum will be zero
+def sum_of_three_numbers(num1, num2, num3):
+
+    if num1 == num2 or num1 == num3 or num2 == num3:
+        return 0
+    else:
+        return num1 + num2 + num3
+
+
+print(sum_of_three_numbers(2, 5, 4))
+print(sum_of_three_numbers(2, 5, 2))
