e9e5f89cd401f70252cc894588963626f59129f9
HannanBhuiyan <hannan.diuh@gmail.com>
2022-04-16 23:26:27 +0600

    some calculation

---
+++ problem35.py
@@ -0,0 +1,8 @@
+# problem : Write a Python program to solve (x + y) * (x + y)
+# output : (4 + 3) ^ 2) = 49
+
+def math_solve(x, y):
+    print(f"({x} + {y}) ^ 2 = ",(x+y)*(x+y))
+
+
+math_solve(4, 3)
